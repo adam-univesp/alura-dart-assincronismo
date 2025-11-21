ee7d6a89973aa877eb8d82b5a8202ed21e1b59f1
adam-univesp <207452333+adam-univesp@users.noreply.github.com>
2025-11-21 16:14:20 +0000

    primeiro commit

---
+++ test/alura_dart_assincronismo_test.dart
@@ -0,0 +1,8 @@
+import 'package:alura_dart_assincronismo/alura_dart_assincronismo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
