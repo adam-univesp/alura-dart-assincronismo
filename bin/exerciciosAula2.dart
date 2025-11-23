a827aee5116ce605298f9730ed625d1453bf85bd
adam-univesp <207452333+adam-univesp@users.noreply.github.com>
2025-11-23 22:20:10 +0000

    Aula 3: Finalizado

--- bin/exerciciosAula2.dart
+++ bin/exerciciosAula2.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 void main() async {
-  print("\n\n Exercicio 1! \n\n");
+  print("\n Exercicio 1! \n");
   List<dynamic> biblioteca = await solicitaJSON(
     "https://raw.githubusercontent.com/alura-cursos/dart_assincronismo_api/aula05/.json/books.json",
   );
@@ -11,6 +11,99 @@ void main() async {
   for (Map<String, dynamic> livro in livros) {
     print("Titulo: ${livro['title']}");
   }
+
+  print("\n Exercicio 2! \n");
+
+  List<String> ingredientes = ["ovo"];
+  List<dynamic> data = await solicitaJSON(
+    "https://raw.githubusercontent.com/alura-cursos/dart_assincronismo_api/aula05/.json/recipes.json",
+  );
+
+  List<Map<String, dynamic>> receitas = List<Map<String, dynamic>>.from(data);
+
+  List<Map<String, dynamic>> receitasFiltradas = filtraReceitas(
+    receitas,
+    ingredientes,
+  );
+
+  print("\n Exercicio 3! \n");
+  Map<String, dynamic> dataMap = await solicitaJSONMap(
+    "https://raw.githubusercontent.com/alura-cursos/dart_assincronismo_api/aula05/.json/players.json",
+  );
+  escalaTime(dataMap);
+
+  print("\n Exercicio 4! \n");
+  data = await solicitaJSON(
+    "https://raw.githubusercontent.com/alura-cursos/dart_assincronismo_api/refs/heads/aula05/.json/vet.json",
+  );
+  String nome = "Dra. Patrícia Gomes";
+  organizadorAgendamento(data, nome);
+}
+
+void organizadorAgendamento(List consultas, String nome) {
+  List consultasFiltradas = consultas
+      .where((consulta) => consulta["veterinarian"] == nome)
+      .toList();
+  consultasFiltradas.sort(
+    (a, b) => DateTime.parse(
+      a["appointment"],
+    ).compareTo(DateTime.parse(b["appointment"])),
+  );
+  for (Map<String, dynamic> consulta in consultasFiltradas) {
+    print(
+      'Nome: ${consulta["pet_name"]}\t--> Horario ${consulta["appointment"]}',
+    );
+  }
+}
+
+void escalaTime(Map<String, dynamic> dataMap) {
+  int jogadoresPorTime = dataMap["rules"]["playersPerTeam"];
+  List<Map<String, dynamic>> jogadores = List<Map<String, dynamic>>.from(
+    dataMap["players"],
+  );
+
+  jogadores.sort(
+    (a, b) =>
+        (b["roundsWaiting"] as int).compareTo((a["roundsWaiting"] as int)),
+  );
+
+  for (int i = 0; i * jogadoresPorTime < jogadores.length; i++) {
+    int end = (i + 1) * jogadoresPorTime;
+    end = end <= jogadores.length ? end : jogadores.length - 1;
+    List<Map<String, dynamic>> time = jogadores.sublist(
+      i * jogadoresPorTime,
+      end,
+    );
+    imprimeTime(i, time);
+  }
+}
+
+void imprimeTime(int i, List<Map<String, dynamic>> time) {
+  print("\n *** Time ${i + 1} ***");
+  for (Map<String, dynamic> jogador in time) {
+    print("Jogador: ${jogador['name']}");
+  }
+}
+
+List<Map<String, dynamic>> filtraReceitas(
+  List<Map<String, dynamic>> receitas,
+  List<String> ingredientes,
+) {
+  List<Map<String, dynamic>> receitasFiltradas = receitas
+      .where(
+        (receita) => ingredientes.every(
+          (ingrediente) => receita["ingredients"].any(
+            (igredienteReceita) => (igredienteReceita as String)
+                .toLowerCase()
+                .contains(ingrediente.toLowerCase()),
+          ),
+        ),
+      )
+      .toList();
+  for (Map<String, dynamic> receita in receitasFiltradas) {
+    print("Receita: ${receita['name']}");
+  }
+  return receitasFiltradas;
 }
 
 List<dynamic> livrosAutor(List<dynamic> biblioteca, String autor) {
@@ -22,4 +115,10 @@ Future<List<dynamic>> solicitaJSON(String url) async {
   Response resultado = await get(uri);
   List<dynamic> listaResultado = json.decode(resultado.body);
   return listaResultado;
+}
+
+Future<Map<String, dynamic>> solicitaJSONMap(String url) async {
+  Uri uri = Uri.parse(url);
+  Response resultado = await get(uri);
+  return json.decode(resultado.body);
 }
--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,11 @@
+import 'dart:io';
+
+import 'package:alura_dart_assincronismo/key.dart';
 import 'package:http/http.dart';
-import 'package:alura_dart_assincronismo/exercicios.dart';
 import 'dart:convert';
 
 void main() async {
-  print("1");
-  requestData();
-  print("2");
-  await readDataAsync();
-  print("3");
+sendDataAsync({"a": 1, "c": 7});
 }
 
 Future<void> requestData() async {
@@ -26,15 +24,33 @@ Future<void> requestData() async {
   print("1.2");
 }
 
-  readDataAsync() async {
+Future<List<dynamic>> readDataAsync() async {
   String url =
       "https://gist.githubusercontent.com/adam-univesp/826ff7f981fe321bf5b03763c1516508/raw/bf34368e8a293ce9e52b17c9ec45056d4529a99a/accounts.json";
   Uri uri = Uri.parse(url);
   Response resposta = await get(uri);
   List<dynamic> listAccount = json.decode(resposta.body);
-  Map<String, dynamic> mapAna = listAccount.firstWhere(
-    (valor) => valor["name"] == "Ana",
+  return listAccount;
+}
+
+sendDataAsync(Map<String, dynamic> account) async {
+  List<dynamic> listAccount = await readDataAsync();
+  print(listAccount.toString());
+  listAccount.add(account);
+  String url = "https://api.github.com/gists/826ff7f981fe321bf5b03763c1516508";
+  Uri uri = Uri.parse(url);
+  Map<String, String> headder = {"Authorization": "Bearer $gist_key"};
+  Map<String, dynamic> body = {
+    "description": "accounts.json",
+    "files": {
+      "accounts.json": {"content": json.encode(listAccount)},
+    },
+  };
+  Response resposta = await post(
+    uri,
+    headers: headder,
+    body: json.encode(body),
   );
-  print(mapAna);
-  print("2.2");
+  print(json.encode(body));
+  print(resposta.statusCode);
 }
